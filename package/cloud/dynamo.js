4911de095bef1a73cd56d266041a0c6d108491c3
janfabian <mail@janfabian.cz>
2021-07-13 16:28:40 +0200

    dynamo tables

--- package/cloud/dynamo.js
+++ package/cloud/dynamo.js
@@ -37,9 +37,50 @@ const workspaceInstallationTable = new aws.dynamodb.Table(
   }
 );
 
+const offtopicChannelsTable = new aws.dynamodb.Table(
+  process.env.DYNAMODB_TABLE_OFFTOPIC_CHANNELS,
+  {
+    attributes: [
+      {
+        name: "teamId",
+        type: "S",
+      },
+    ],
+    billingMode: "PAY_PER_REQUEST",
+    hashKey: "teamId",
+    tags: {
+      Environment: pulumi.getStack(),
+    },
+  }
+);
+
+const threadsTable = new aws.dynamodb.Table(
+  process.env.DYNAMODB_TABLE_THREADS,
+  {
+    attributes: [
+      {
+        name: "teamId",
+        type: "S",
+      },
+      {
+        name: "messageId",
+        type: "S",
+      },
+    ],
+    billingMode: "PAY_PER_REQUEST",
+    hashKey: "teamId",
+    rangeKey: "messageId",
+    tags: {
+      Environment: pulumi.getStack(),
+    },
+  }
+);
+
 module.exports.migrationsTable = migrationsTable;
 module.exports.workspaceInstallationTable = workspaceInstallationTable;
 module.exports.DYNAMODB_TABLES = {
   DYNAMODB_TABLE_MIGRATIONS: migrationsTable,
   DYNAMODB_TABLE_WORKSPACE_INSTALLATIONS: workspaceInstallationTable,
+  DYNAMODB_TABLE_OFFTOPIC_CHANNELS: offtopicChannelsTable,
+  DYNAMODB_TABLE_THREADS: threadsTable,
 };
